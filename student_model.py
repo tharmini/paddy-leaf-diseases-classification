e14a0603e898f36151122a5ef2c0b78cc7e0590a
Tharmini Thalayasingam <tharmini7@gmail.com>
2026-01-31 15:33:37 +0530

    Update student_model.py

--- student_model.py
+++ student_model.py
@@ -313,7 +313,7 @@ def build_fpn_hydranet_model(
 student_model = build_fpn_hydranet_model()
 
 student_model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
     metrics=['accuracy']
 )
