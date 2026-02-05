61c3aed5d139129c24f341eedea8367bace18c9e
Tharmini Thalayasingam <tharmini7@gmail.com>
2026-02-05 11:52:39 +0530

    Update teacher_model.py

--- teacher_model.py
+++ teacher_model.py
@@ -320,7 +320,7 @@ def build_fpn_hydranet_model(
 model = build_fpn_hydranet_model()
 
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.00005),
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
     metrics=['accuracy']
 )
